17d9a53ee3e0a8aedee1e36222accf9111a46892
wnsgud0428 <wnsgud0428@naver.com>
2022-02-21 20:16:53 +0900

    4.4 전체 조회 화면 만들기

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,13 +1,26 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
     }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
 }
